b68ef89cda725b49af192ec72bff170c47038247
Keven da Silva Gonçalves <dasilvagkeven@gmail.com>
2023-07-04 17:14:42 -0300

    update: parkings index

--- app/helpers/parkings_helper.rb
+++ app/helpers/parkings_helper.rb
@@ -1,2 +1,15 @@
 module ParkingsHelper
+  def format_time(time_string)
+    time = Time.parse(time_string)
+    time.strftime("%H:%M")
+  end
+
+  def format_cost(cost)
+    number_with_precision(cost, precision: 2)
+  end
+
+  def format_date(date_string)
+    date = Date.parse(date_string)
+    date.strftime("%d/%m/%Y")
+  end
 end
